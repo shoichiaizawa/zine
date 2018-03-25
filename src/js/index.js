71f50ca9f1b3c54d204bd12c471f7c62f77661e6
大東祐太 <h-1456@H-1456-ohigashi-yuta.local>
2018-03-25 09:31:28 +0900

    update: いろいろー(ごめんなさい)

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,102 @@
 import { h, app } from "hyperapp"
 import { Link, Route, location } from "@hyperapp/router"
 
+const PAGE_MAX = 10
+const CANVAS_RATE = 0.9
+
 const canvas = {
-  height: 525 * 1.4,
-  width: 370 * 1.4 * 2,
+  height: 841 * CANVAS_RATE,
+  width: 594 * CANVAS_RATE * 2,
   color: "rgba(0, 0, 0, 0.9)",
   edgeSize: 100
 }
 
+const zines = ["phigasui", "curry", "tokuyama"]
+
 const Home = () => (
   <div>
-    <h2>Homee</h2>
+    <h2>Home</h2>
     <p>IdeaZineではかんたんにZineを作ってWebで公開できるよ。ほしければ印刷もできるよ(予定)</p>
   </div>
 )
-const Read = () => (
+
+const Collection = () => (
+  <div style={{
+         display: "table"
+       }} >
+    {zines.map((zine) => (
+      <ZineCover zineName={zine} />
+    ))}
+  </div>
+)
+
+const ZineCover = ({zineName}) => (state, actions) => (
+  <Link to="/read">
+    <div style={{
+           display: "table-cell",
+           width: 300,
+           border: "1px solid #ddd",
+           "border-radius": "10px",
+           cursor: "pointer"
+         }}
+         onclick={e => {
+           actions.loadState(zineName+".json")
+      }} >
+      <img style={{
+             width: 200,
+             padding: 30,
+             "margin-left": "auto",
+             "margin-right": "auto"
+           }}
+           src={"./images/"+zineName+".png"} />
+    </div>
+  </Link>
+)
+
+const DownloadpPages = (state) => (
+  <div>
+    <h2>DownloadPage</h2>
+    <input type="button" onclick={() => {
+        const element = document.getElementsByClassName('canvas')[0];
+
+        html2pdf(element, {
+          margin:       1,
+          filename:     'myfile.pdf',
+          image:        { type: 'jpeg', quality: 0.98 },
+          html2canvas:  { dpi: 192, letterRendering: true },
+          jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+        })
+      }} />
+    {state.pages.map((page) => {
+        console.log(page)
+    })}
+  </div>
+)
+
+const Read = () => (state, actions) => (
   <div>
     <h2>Read</h2>
-    <Page/>
+    <Link to="/edit">Edit</Link>
+    <Page edit={false} />
   </div>
 )
 const Topic = ({ match }) => (
   <h2>{match.params.topicId}</h2>
 )
-const Edit = ({ match }) => (
+const Edit = ({ match }) => (state, actions) => (
   <div>
     <h2>Edit</h2>
     <p>ドラッグ&ドロップで画像を入れられるよ</p>
     <p>テキストはテキストボックスから入れてね</p>
+    <a download={true}
+       href={
+         (
+           "data:text/json;charset=utf-8,"
+             + encodeURIComponent(JSON.stringify(state.pages))
+         )
+       } >
+      JSONダウンロード
+    </a>
     <Page edit={true} />
   </div>
 )
@@ -54,16 +123,16 @@ const TopicsView = ({ match }) => (
 const Zine = () => (state, actions) => (
   <Page edit={false} />
 )
-const Page = ({edit}) => (state, actions) => (
+const Page = ({edit, page}) => (state, actions) => (
   <main>
-    <p>ページ:{state.pageNum}/{edit ? state.pageMax : state.pages.length}</p>
+    <p>ページ:{page ? page.pageNum : state.pageNum}/{edit ? PAGE_MAX : state.pages.length}</p>
     <div style={{
            display: edit ? "block" : "none"
          }}>
-      <input type="text" value={state.inputText} oninput={e => {
+      <textarea value={state.inputText} oninput={e => {
           actions.setText(e.target.value)
         }} >
-      </input>
+      </textarea>
       <input type="button" value="テキスト挿入" onmouseup={e => {
           actions.addText()
           actions.savePage()
@@ -80,7 +149,14 @@ const Page = ({edit}) => (state, actions) => (
              background: canvas.color,
              position: "absolute",
              "z-index": 1
-           }} />
+           }} >
+        <p style={{
+             color: "#FFF",
+             display: edit && state.pageNum == 1 ? "block" : "none"
+           }} >
+          カバーになるページです。◀裏表紙----表紙▶です。お気をつけください。
+        </p>
+      </div>
       <div Class="edge left" style={{
              top: canvas.edgeSize,
              height: canvas.height,
@@ -95,10 +171,10 @@ const Page = ({edit}) => (state, actions) => (
              width: "auto",
              margin: 30,
              cursor: "pointer",
-             display: state.pageNum > 1 ? "block" : "none"
+             display: (page ? page.pageNum : state.pageNum) > 1 ? "block" : "none"
           }}
           onclick={e => {
-            actions.loadPage(state.pageNum - 1)
+            actions.loadPage((page ? page.pageNum : state.pageNum) - 1)
           }}>
           ◀
         </p>
@@ -109,8 +185,7 @@ const Page = ({edit}) => (state, actions) => (
              height: canvas.height,
              left: canvas.edgeSize,
              top: canvas.edgeSize
-           }}
-           >
+           }} >
         <div style={{
                position: "absolute",
                left: canvas.width / 2,
@@ -131,6 +206,14 @@ const Page = ({edit}) => (state, actions) => (
                   offsetY: e.pageY - image.top,
                   target: "images"
                 })
+                if (state.selected.object == "image" && state.selected.id == image.id) {
+                  actions.unselect()
+                } else {
+                  actions.select({
+                    object: "image",
+                    id: image.id
+                  })
+                }
               }
             }}
             style={{
@@ -139,7 +222,8 @@ const Page = ({edit}) => (state, actions) => (
               top: image.top,
               cursor: edit ? "move" : "init",
               position: "absolute",
-              transform: "rotate(" + image.deg + "deg)"
+              transform: "rotate(" + image.deg + "deg)",
+              border: state.selected.object == "image" && state.selected.id == image.id ? "2px solid #ddd" : 0
             }} />
         ))}
   {state.texts.map((text) => (
@@ -151,7 +235,8 @@ const Page = ({edit}) => (state, actions) => (
          width: text.width,
          cursor: edit ? "move" : "init",
          position: "absolute",
-         transform: "rotate(" + text.deg + ")"
+         transform: "rotate(" + text.deg + ")",
+         border: state.selected.object == "text" && state.selected.id == text.id ? "2px solid #ddd" : 0
        }}
        onmousedown={e => {
          if (edit) {
@@ -161,6 +246,14 @@ const Page = ({edit}) => (state, actions) => (
              offsetY: e.pageY - text.top,
              target: "texts"
            })
+           if (state.selected.object == "text" && state.selected.id == text.id) {
+             actions.unselect()
+           } else {
+             actions.select({
+               object: "text",
+               id: text.id
+             })
+           }
          }
       }}
       >
@@ -183,10 +276,10 @@ const Page = ({edit}) => (state, actions) => (
       color: "#FFF",
       margin: 30,
       cursor: "pointer",
-      display: state.pageNum < (edit ? state.pageMax : state.pages.length) ? "block" : "none"
+      display: (page ? page.pageNum : state.pageNum) < (edit ? PAGE_MAX : state.pages.length) ? "block" : "none"
     }}
   onclick={e => {
-    actions.loadPage(state.pageNum + 1)
+    actions.loadPage((page ? page.pageNum : state.pageNum) + 1)
   }}
     >
     ▶
@@ -208,12 +301,15 @@ const Page = ({edit}) => (state, actions) => (
 const state = {
   location: location.state,
   pageNum: 1,
-  pageMax: 4,
   texts: [],
   images: [],
   dragData: {id: null, offsetX: null, offsetY: null, target: null},
   inputText: null,
-  pages: []
+  pages: [],
+  selected: {
+    object: null,
+    id: null
+  }
 }
 
 const existPage = (state, pageNum) => {
@@ -243,6 +339,8 @@ const getPageData = (state, pageNum) => {
 const actions = {
   location: location.actions,
   drop: () => ({dragData: {id: null}}),
+  select: (data) => ({selected: data}),
+  unselect: () => ({selected: {object: null, id: null}}),
   drag: (data) => ({dragData: data}),
   move: (data) => state => (moveData(state, data)),
   addImage: (src) => state => ({images: state.images.concat(
@@ -268,7 +366,25 @@ const actions = {
     }
   )}),
   savePage: () => state => ({pages: getUpdatedPages(state)}),
-  loadPage: (pageNum) => state => (getPageData(state, pageNum))
+  loadPage: (pageNum) => state => (getPageData(state, pageNum)),
+  keyType: (key) => state => (getUpdatedStateByKey(state, key)),
+  loadState: (file) => (state, actions) => {loadJSON(file, actions)},
+  setState: (data) => state => (data),
+  init: () => state => ({pages: [], texts: [], images: [], pageNum: 1})
+}
+
+const loadJSON = (file, actions) => {
+  console.log("loading")
+  fetch("./data/"+file, {mode: 'cors'})
+    .then(data => data.json())
+    .then(data => {
+      actions.setState({
+        pageNum: 1,
+        pages: data,
+        images: data[0].images,
+        texts: data[0].texts
+      })
+    })
 }
 
 const getUpdatedPages = (state) => {
@@ -288,31 +404,98 @@ const getUpdatedPages = (state) => {
     updatedPages[index] = page
   }
 
-  console.log(updatedPages)
-
   return updatedPages
 }
 
-const view = state => (
+const getUpdatedStateByKey = (state, key) => {
+  const object = state.selected.object + 's'
+  const objectId = state.selected.id
+
+  var updatedState = state
+
+  if (objectId == null) {return updatedState}
+
+
+  if (!['d', 'k', 'j', 'u', 'l'].find(k => k == key)) {
+    return updatedState
+  }
+
+  const index = state[object].findIndex(i => i.id == objectId)
+
+  if (key == 'd') {
+    console.log(index)
+    updatedState[object].splice(index, 1)
+  } else if (key == 'k') {
+    updatedState[object][index].width += 1
+  } else if (key == 'j') {
+    updatedState[object][index].width -= 1
+  } else if (key == 'u') {
+    updatedState[object][index].fontSize += 1
+  } else if (key == 'l') {
+    updatedState[object][index].fontSize -= 1
+  }
+
+  return updatedState
+}
+
+const view = (state, actions) => (
   <div>
-    <Link to="/">
+    <Link to="/" style={{
+            "text-decoration": "none"
+          }}>
       <h1>IdeaZine</h1>
     </Link>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/read">Read</Link>
-      </li>
-      <li>
-        <Link to="/edit">Edit</Link>
-      </li>
-    </ul>
+    <div style={{
+           "border-collapse": "separate",
+           "border-spacing": 8
+         }}>
+      <Link to="/">
+        <span style={{
+                display: "table-cell",
+                width: 110,
+                height: 68,
+                "vertical-align": "middle",
+                "border-style": "solid",
+                "border-width": 1.5,
+                "border-color": "rgba(0,0,0,0.8)",
+                "border-radius": "10px",
+                "text-align": "center"
+              }} >Home</span>
+      </Link>
+      <Link to="/collection">
+        <span style={{
+                display: "table-cell",
+                width: 110,
+                height: 68,
+                "vertical-align": "middle",
+                "border-style": "solid",
+                "border-width": 1.5,
+                "border-color": "rgba(0,0,0,0.8)",
+                "border-radius": "10px",
+                "text-align": "center"
+              }}>Collection</span>
+      </Link>
+      <Link to="/edit">
+        <span style={{
+                display: "table-cell",
+                width: 110,
+                height: 68,
+                "vertical-align": "middle",
+                "border-style": "solid",
+                "border-width": 1.5,
+                "border-color": "rgba(0,0,0,0.8)",
+                "border-radius": "10px",
+                "text-align": "center"
+              }}
+              onclick={e => (actions.init())}
+              >Create</span>
+      </Link>
+    </div>
 
-    <Route path="/" render={Edit} />
+    <Route path="/" render={Collection} />
     <Route path="/read" render={Read} />
     <Route path="/edit" render={Edit} />
+    <Route path="/collection" render={Collection} />
   </div>
 )
 
@@ -326,11 +509,9 @@ addEventListener("touchmove", e => main.move({ x: e.pageX, y: e.pageY }))
 addEventListener("drop", e => {
   e.preventDefault()
   var files = e.dataTransfer.files
-  console.log(e.pageX, e.pageY)
 
   for(var i=0; i<files.length; i++) {
     var file = files[i]
-    console.log(file)
 
     var reader = new FileReader()
     reader.onload = function(e) {
@@ -340,6 +521,11 @@ addEventListener("drop", e => {
     }
     reader.readAsDataURL(file)
   }
+})
+
+addEventListener("keypress", e => {
+  main.keyType(e.key)
+  main.savePage()
 })
 
 addEventListener("dragover", e => (e.preventDefault()))
